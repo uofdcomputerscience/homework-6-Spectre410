f9214ac789de26e88d3450a943b1521f8dcf6f36
Matthew Pritchett <mpritchett50@gmail.com>
2019-10-31 08:23:55 -0500

    Added functionality so that the continue button is enabled

--- Homework6/Homework6/ViewController.swift
+++ Homework6/Homework6/ViewController.swift
@@ -15,13 +15,39 @@ class ViewController: UIViewController {
     @IBOutlet var selectionButton: UIButton!
     @IBOutlet var continueButton: UIButton!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
     @IBAction func segmentedControllerDidChangeValue(_ sender: UISegmentedControl) {
+        if(selectionButton.isSelected){
+            if(segmentedController.selectedSegmentIndex == 2){
+                if(selectionSwitch.isOn){
+                    continueButton.isEnabled = true
+                }
+            }
+        }
     }
     
     @IBAction func selectionSwitchDidChangeValue(_ sender: UISwitch) {
+        if(selectionButton.isSelected){
+            if(segmentedController.selectedSegmentIndex == 2){
+                if(selectionSwitch.isOn){
+                    continueButton.isEnabled = true
+                }
+            }
+        }
+        
     }
     
     @IBAction func selectionButtonTapped(_ sender: UIButton) {
+        selectionButton.isSelected = true
+        if(segmentedController.selectedSegmentIndex == 2){
+            if(selectionSwitch.isOn){
+                    continueButton.isEnabled = true
+            }
+        }
     }
 }
+
 
